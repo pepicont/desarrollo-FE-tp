b4b86ec4d7e1356787f80aa1ca70c7c566be956b
pepicont <stefano.conti2004@gmail.com>
2025-08-14 12:17:03 -0300

    probé el import del login template y anduvo

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import SignIn from './sign-in/SignIn.tsx'
+//import App from './App.tsx'+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SignIn />
   </StrictMode>,
 )
