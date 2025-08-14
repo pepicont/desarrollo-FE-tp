a787695fdde031aa43f0cc1c80dc283d7acdc961
pepicont <stefano.conti2004@gmail.com>
2025-08-14 11:24:00 -0300

    volví a hacer app, se rompio por todos lados

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-//import App from './App.tsx'
-import MarketingPage from './marketing-page/MarketingPage.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MarketingPage />
+    <App />
   </StrictMode>,
 )
